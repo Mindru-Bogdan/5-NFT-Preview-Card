3057d7b48b99ea1791b54e0d020cf746875d5844
Bogdan Mindru <bogdan.mindru97@gmail.com>
2023-05-05 13:29:13 +0300

    Post initial files

---
+++ src/Components/Card.js
@@ -0,0 +1,17 @@
+import classes from './Card.module.css';
+
+const Card = () => {
+  return (
+    <div>
+      <h1>Equilibrium #3429</h1>
+      <p>Our Equilibrium collection promotes balance and calm.</p>
+      <div>
+        <h3>0.041 ETH</h3>
+        <p>3 days left</p>
+      </div>
+      <h5>Creation of Jules Wyvern</h5>
+    </div>
+  );
+};
+
+export default Card;
