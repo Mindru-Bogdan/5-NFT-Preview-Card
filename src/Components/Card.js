821cb550be9fd1140d820b998e43476312504c9e
Bogdan Mindru <bogdan.mindru97@gmail.com>
2023-05-05 20:04:09 +0300

    Device Friendly Solution

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,15 +1,30 @@
+import ImgEquilibrium from '../../src/image-equilibrium.jpg';
+import IconView from '../../src/icon-view.svg';
+import IconEthereum from '../../src/icon-ethereum.svg';
+import IconClock from '../../src/icon-clock.svg';
+import ImgAvatar from '../../src/image-avatar.png';
+
 import classes from './Card.module.css';
 
 const Card = () => {
   return (
-    <div>
-      <h1>Equilibrium #3429</h1>
+    <div className={classes.card}>
+      <img className={classes.equilibrium} src={ImgEquilibrium}></img>
+      <img className={classes.overlay} src={IconView}></img>
+      <h2>Equilibrium #3429</h2>
       <p>Our Equilibrium collection promotes balance and calm.</p>
-      <div>
+      <div className={classes.price}>
+        <img className={classes.ethereum} src={IconEthereum}></img>
         <h3>0.041 ETH</h3>
+        <img className={classes.clock} src={IconClock}></img>
         <p>3 days left</p>
       </div>
-      <h5>Creation of Jules Wyvern</h5>
+      <div className={classes.line}></div>
+      <div className={classes.footer}>
+        <img src={ImgAvatar}></img>
+        <h4>Creation of</h4>
+        <p>Jules Wyvern</p>
+      </div>
     </div>
   );
 };
